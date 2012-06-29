0404ea44d427d8d6e8155eac2a549f06cd6c1a3a
Sam <sam@cruft.co>
2012-06-29 09:24:24 +0100

    Reformat test code

--- spec/insulin_spec.rb
+++ spec/insulin_spec.rb
@@ -44,23 +44,54 @@ describe Insulin::OnTrackNote do
 end
 
 describe Insulin::OnTrackNoteSet do
-  simple_note_set = Insulin::OnTrackNoteSet.new ["F: turkey breast, salad"]
+  simple_note_set = Insulin::OnTrackNoteSet.new [
+    "F: turkey breast, salad"
+  ]
 
   it "get simple note set" do
-    simple_note_set.should == {"food" => ["turkey breast", "salad"]}
+    simple_note_set.should == {
+      "food" => [
+        "turkey breast",
+        "salad"
+      ]
+    }
   end
 
-  multiple_note_set = Insulin::OnTrackNoteSet.new ["F: turkey breast, salad", "N:After wine. No overnight hypo.", "N:some other note", "B: glass of wine"]
+  multiple_note_set = Insulin::OnTrackNoteSet.new [
+    "F: turkey breast, salad",
+    "N:After wine. No overnight hypo.",
+    "N:some other note",
+    "B: glass of wine"
+  ]
 
   it "get multiple note set" do
-    multiple_note_set.should == {"food" => ["turkey breast", "salad"], "note" => ["after wine. no overnight hypo.", "some other note"], "booze" => ["glass of wine"]}
+    multiple_note_set.should == {
+      "food" => [
+        "turkey breast",
+        "salad"
+      ],
+      "note" => [
+        "after wine. no overnight hypo.",
+        "some other note"
+      ],
+      "booze" => [
+        "glass of wine"
+      ]
+    }
   end
 
-#  failing_note_set = Insulin::OnTrackNoteSet.new ["F: turkey breast, salad", "X:This should fail"]
-#
-#  it "get failing note set" do
-#    failing_note_set.should == {"food" => ["turkey breast", "salad"]}
-#  end
+  failing_note_set = Insulin::OnTrackNoteSet.new [
+    "F: turkey breast, salad", "X:This should fail"
+  ]
+
+  it "get failing note set" do
+    failing_note_set.should == {
+      "food" => [
+        "turkey breast",
+        "salad"
+      ]
+    }
+  end
 end
 
 describe Insulin::OnTrackCsvLine do
